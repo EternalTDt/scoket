c14e86545b6b6ba0b751d41386ac0c697ca8c6a1
Tim <demidovbest@gmail.com>
2022-01-27 15:40:26 +0300

    update whitenoise storage path

--- socketstore/settings.py
+++ socketstore/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
