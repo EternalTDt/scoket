4a5137dae6cb61f66627f9450c0c8e2c16e89580
Tim <demidovbest@gmail.com>
2022-01-31 19:36:23 +0300

    added ckeditor

--- fakeapi/admin.py
+++ fakeapi/admin.py
@@ -1,9 +1,21 @@
+from dataclasses import field
 from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from .models import (
      FirstLevelCategory, 
      SecondLevelCategory, 
-     ThirdLevelCategory
+     ThirdLevelCategory,
+     Brand,
 )
+
+
+class BrandAdminForm(forms.ModelForm):
+    description = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Brand
+        fields = ('__all__')
 
 
 @admin.register(FirstLevelCategory)
@@ -31,3 +43,12 @@ class ThirdLevelCategoryAdmin(admin.ModelAdmin):
     search_fields = ('name', 'category__name')
     list_filter = ('category',)
     prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+    form = BrandAdminForm
