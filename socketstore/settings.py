4c95c7fdcaf34dec68800a95315b1f8767055953
Tim Demidov <demidovbest@gmail.com>
2022-05-17 13:11:59 +0300

    add auth.token

--- socketstore/settings.py
+++ socketstore/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',
 
     # project apps #
     'core.apps.CoreConfig',
@@ -116,6 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
--- socketstore/urls.py
+++ socketstore/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('ckeditor/', include('ckeditor_uploader.urls')),
 	# djoser
     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
     # accounts
     path('accounts/api/',include("accounts.urls")),
